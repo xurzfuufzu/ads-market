dc3218ab9081bdf39d5a575e5d6074eca886e005
xurzfuufzu <joodikstyle@gmail.com>
2025-05-09 01:07:43 +0500

    deleted

--- internal/transport/http/ad.go
+++ internal/transport/http/ad.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"Ads-marketplace/internal/domain/ad"
 	"Ads-marketplace/internal/service"
-	"Ads-marketplace/pkg/utils/response"
 	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v3"
@@ -27,22 +26,28 @@ func NewAdHandler(adService *service.AdService) *AdHandler {
 // @Accept  json
 // @Produce  json
 // @Param request body ad.CreateRequest true "Ad Creation Request"
-// @Success 200 {object} response.Response{Message="Ad created successfully"}
-// @Failure 400 {object} response.Response{Error="Invalid request"}
-// @Failure 500 {object} response.Response{Error="Internal server error"}
+// @Success 200 {object} response.Message
+// @Failure 400 {object} response.Error
+// @Failure 500 {object} response.Error
 // @Router /ad/create [post]
 func (h *AdHandler) Create(c fiber.Ctx) error {
 	var input *ad.CreateRequest
 	if err := json.Unmarshal(c.Body(), &input); err != nil {
 		fmt.Println(err)
-		return c.Status(http.StatusBadRequest).JSON(response.ClientResponse(http.StatusBadRequest, "Invalid request", nil, err.Error()))
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid request",
+		})
 	}
 
 	if err := h.adService.CreateAd(c.Context(), input); err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(response.ClientResponse(http.StatusInternalServerError, "Internal server error", nil, err.Error()))
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
 	}
 
-	return c.Status(http.StatusOK).JSON(response.ClientResponse(http.StatusOK, "Ad created successfully", nil, nil))
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "Ad created successfully",
+	})
 }
 
 // GetAllAds handles fetching all advertisements.
@@ -51,15 +56,17 @@ func (h *AdHandler) Create(c fiber.Ctx) error {
 // @Tags Ad
 // @Produce  json
 // @Success 200 {array} ad.Ad "List of advertisements"
-// @Failure 500 {object} response.Response{Error="Internal server error"}
+// @Failure 500 {object} fiber.Map{"error": "Internal server error"}
 // @Router /ad/all [get]
 func (h *AdHandler) GetAllAds(c fiber.Ctx) error {
 	ads, err := h.adService.GetAllAds(c.Context())
 	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(response.ClientResponse(http.StatusInternalServerError, "Internal server error", nil, err.Error()))
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
 	}
 
-	return c.Status(http.StatusOK).JSON(response.ClientResponse(http.StatusOK, "Success", ads, nil))
+	return c.Status(http.StatusOK).JSON(ads)
 }
 
 // DeleteByID handles the deletion of an advertisement by ID.
@@ -67,18 +74,22 @@ func (h *AdHandler) GetAllAds(c fiber.Ctx) error {
 // @Description This endpoint deletes an advertisement from the marketplace by its ID.
 // @Tags Ad
 // @Param id path string true "Ad ID"
-// @Success 200 {object} response.Response{}
-// @Failure 400 {object} response.Response{Error="ID is required"}
-// @Failure 500 {object} response.Response{Error="Internal server error"}
+// @Success 200 {object} fiber.Map{}
+// @Failure 400 {object} fiber.Map{"error": "ID is required"}
+// @Failure 500 {object} fiber.Map{"error": "Internal server error"}
 // @Router /ad/delete/{id} [delete]
 func (h *AdHandler) DeleteByID(c fiber.Ctx) error {
 	id := c.Params("id")
 	if id == "" {
-		return c.Status(http.StatusBadRequest).JSON(response.ClientResponse(http.StatusBadRequest, "ID is required", nil, nil))
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": "ID is required",
+		})
 	}
 	err := h.adService.DeleteAdByID(c.Context(), id)
 	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON(response.ClientResponse(http.StatusInternalServerError, "Internal server error", nil, err.Error()))
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
 	}
-	return c.Status(http.StatusOK).JSON(response.ClientResponse(http.StatusOK, "Success", nil, nil))
+	return c.Status(http.StatusOK).JSON(fiber.Map{})
 }
